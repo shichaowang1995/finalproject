fb479c18edbe9a733c492e5a798843b9f4669005
Carly <wangshichao1995@gmail.com>
2019-05-03 19:38:10 -0400

    submitsurvey
    the function to assess how many box are checked added

---
+++ main.js
@@ -0,0 +1,22 @@
+  var that = this;
+
+function submitAnswer(){
+  var checkboxes = document.getElementsByName("challenge");
+  var form = document.getElementById('theForm')
+  var numberOfCheckedItems = 0;
+  for(var i = 0; i < checkboxes.length; i++){
+    if(checkboxes[i].checked)
+    numberOfCheckedItems++;
+  }
+  if(numberOfCheckedItems == 0){
+    alert("You must select at least 1 challenge");
+    return false;
+  }
+  if(numberOfCheckedItems > 3){
+    alert("You can't select more than 3 challenges");
+    return false;
+  } else {
+    form.submit();
+  }
+
+}
